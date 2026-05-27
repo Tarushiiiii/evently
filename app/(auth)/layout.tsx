8e64d4517755e7eef16ab943776bc09143eb9f78
Tarushi Agarwal <agarwaltaru6@gmail.com>
2026-05-28 01:06:20 +0530

    added authentication layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex-center min-h-screen items-center justify-center w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
