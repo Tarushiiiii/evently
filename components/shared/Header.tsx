f67c8923bb0de52efea7c9374e0058d5668e8da9
Tarushi Agarwal <agarwaltaru6@gmail.com>
2026-05-29 01:11:01 +0530

    initiated header components for different screens

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 import { Button } from "../ui/button";
+import { UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = async () => {
   const { userId } = await auth();
@@ -17,12 +20,22 @@ const Header = async () => {
             alt="Evently Logo"
           />
         </Link>
-
+        {userId && (
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        )}
         {!userId && (
           <div className="flex w-32 justify-end gap-3">
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">Login</Link>
             </Button>
+          </div>
+        )}
+        {userId && (
+          <div className="flex w-32 justify-end gap-3">
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </div>
         )}
       </div>
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavItems = () => {
+  return <div>Nav Items</div>;
+};
+
+export default NavItems;
