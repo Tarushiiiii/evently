5453b7cee96134d3e639c39c6869224e2c696cf8
Tarushi Agarwal <agarwaltaru6@gmail.com>
2026-07-15 21:10:09 +0530

    eslint fix

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,35 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongoose ?? {
+  conn: null,
+  promise: null,
+};
+
+global.mongoose = cached;
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
       dbName: "evently",
       bufferCommands: false,
     });
+  }
 
   cached.conn = await cached.promise;
 
